8b637c3072a720ebe0e3742ee95a1a349a20f623
josht <josht@aleios.com>
2023-02-11 16:52:19 +0000

    feat: added crud endpoint

--- handler.js
+++ handler.js
@@ -1,10 +1,25 @@
-module.exports.hello = async () => ({
-  statusCode: 200,
-  body: JSON.stringify(
-    {
-      message: 'Hello from building 16!',
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+
+module.exports.hello = async (event) => {
+  const ddbClient = new DynamoDBClient({region: 'us-east-1'});
+
+  const params = {
+    TableName: "Josh-Hackathon-Table",
+    Item: {
+      name: { S: JSON.parse(event.body).name},
     },
-    null,
-    2,
-  ),
-});
+  };
+
+  const data = await ddbClient.send(new PutItemCommand(params));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'Hello from building 16!',
+        data
+      },
+      null,
+      2,
+    ),
+  }
+};
